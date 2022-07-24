a97a0a2a1fd19bcf93e924d8a2f981c9d34e7a56
mazharul007 <mohammad.mazharul.hoque7@gmail.com>
2022-07-24 14:51:49 +0600

    intro section finished

---
+++ 1_intro.js
@@ -0,0 +1,26 @@
+console.log("Hello JS");
+
+// string concat , int float parseInt parseFloat type conversion 
+var firstName = "Hello";
+var lastName  = "JS";
+console.log( firstName + " " +lastName);
+console.log(`${firstName} ${lastName}`);
+
+var eggPrice = "12";
+console.log(typeof(parseInt(eggPrice)));
+
+// use of toFixed with parseFloat 
+
+var numOne = 0.1;
+var numTwo = 0.2;
+var total = numOne + numTwo;
+console.log(parseFloat(total.toFixed(1)));
+
+
+
+
+
+
+
+
+
